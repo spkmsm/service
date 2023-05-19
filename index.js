9e890b38efb248a05dce3cc2338acd4221debcd0
spkmsm <spksystems003@gmail.com>
2023-05-19 15:54:03 +0530

    changed

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is my about route..... ");
+});
